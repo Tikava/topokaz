5088e34e8e3ea47a4cdd2f59ecb32d43b37344db
Tikava <wavoleniu@gmail.com>
2024-04-16 12:15:49 +0500

    Add config

---
+++ config/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../legends/templates/legends/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
