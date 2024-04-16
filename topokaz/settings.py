8336dfef4f8c5a05b479caf93c8244a8ba31fa1d
Tikava <wavoleniu@gmail.com>
2024-04-16 12:16:32 +0500

    Update settings.py: add static dirs

--- topokaz/settings.py
+++ topokaz/settings.py
@@ -116,11 +116,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
